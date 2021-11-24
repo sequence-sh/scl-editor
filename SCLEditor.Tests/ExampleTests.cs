f1596f2ac0267ad53fc159892e5a206503394a2d
Mark Wainwright <wainwrightml@gmail.com>
2021-11-24 14:39:42 +0000

    Added test for step summaries

---
+++ SCLEditor.Tests/ExampleTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+using Reductech.Utilities.SCLEditor;
+using Xunit;
+
+namespace SCLEditor.Tests;
+
+public class ExampleTests
+{
+    [Fact]
+    public void TestSCLExamples()
+    {
+        var examples = ExampleHelpers.AllExamples.ToList();
+
+        examples.Count.Should().BeGreaterOrEqualTo(2);
+    }
+
+    [Fact]
+    public void TestDocumentation()
+    {
+        var logSummary = new Log<int>().StepFactory.Summary;
+
+        logSummary.Should().NotBeNullOrWhiteSpace();
+
+        var printSummary = new Print<int>().StepFactory.Summary;
+
+        printSummary.Should().NotBeNullOrWhiteSpace();
+    }
+}
--- SCLEditor.Tests/FileSystemTests.cs
+++ SCLEditor.Tests/FileSystemTests.cs
@@ -1,22 +1,8 @@
 ﻿using System.IO.Abstractions.TestingHelpers;
-using System.Linq;
-using FluentAssertions;
-using Reductech.Utilities.SCLEditor;
 using Xunit;
 
 namespace SCLEditor.Tests
 {
-
-public class ResourceTests
-{
-    [Fact]
-    public void TestSCLExamples()
-    {
-        var examples = ExampleHelpers.AllExamples.ToList();
-
-        examples.Count.Should().BeGreaterOrEqualTo(2);
-    }
-}
 
 public class FileSystemTests
 {
