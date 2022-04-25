9286660a8c0b1eed2fea1698219683a684a0729a
antonyoni <antony@reduc.tech>
2022-04-25 15:03:25 +0100

    Enable run to test component. Register as application instead of javascript

--- SCLEditor.React/Program.cs
+++ SCLEditor.React/Program.cs
@@ -4,13 +4,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Reductech.Utilities.SCLEditor.Components;
+using Reductech.Utilities.SCLEditor.React;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.RootComponents.RegisterForJavaScript<Playground>(
-    "scl-playground-react",
-    "sclPlaygroundInit"
-);
+builder.RootComponents.Add<PlaygroundReact>("#scl-playground-root");
+
+//builder.RootComponents.RegisterForJavaScript<PlaygroundReact>(
+//    "scl-playground-react",
+//    "sclPlaygroundInit"
+//);
 
 builder.Services.AddBlazorDownloadFile();
 builder.Services.AddBlazoredLocalStorage();
