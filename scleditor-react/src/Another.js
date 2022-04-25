e3f0bf9880dccd08cfd5781ce45a40b09f6e2133
antonyoni <antony@reduc.tech>
2022-04-25 15:24:16 +0100

    Add additonal page for testing

---
+++ scleditor-react/src/Another.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Another = () => <div>This is another page...</div>;
+
+export default Another;
--- scleditor-react/src/index.js
+++ scleditor-react/src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Another from './Another';
+import { SCLPlayground } from './SCLPlayground';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5, border: 'solid 1px' }}>
+          Playground
+        </Link>
+        <Link to="/another" style={{ padding: 5, border: 'solid 1px' }}>
+          Another Page
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<SCLPlayground />} />
+        <Route path="/another" element={<Another />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
