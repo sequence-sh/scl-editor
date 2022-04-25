bcded31dfe002103d3289bf71b99b66267ac641d
antonyoni <antony@reduc.tech>
2022-04-25 15:23:00 +0100

    Remove css folder

--- run-react.ps1
+++ run-react.ps1
@@ -1,6 +1,6 @@
-Remove-Item -rec -for ./publish, ./scleditor-react/public/_content, ./scleditor-react/public/_framework, ./scleditor-react/public/css -ErrorAction SilentlyContinue
+Remove-Item -rec -for ./publish, ./scleditor-react/public/_content, ./scleditor-react/public/_framework -ErrorAction SilentlyContinue
 dotnet publish --output ./publish ./SCLEditor.React/SCLEditor.React.csproj
-Move-Item ./publish/wwwroot/_content, ./publish/wwwroot/_framework, ./publish/wwwroot/css ./scleditor-react/public/ -ErrorAction Stop
+Move-Item ./publish/wwwroot/_content, ./publish/wwwroot/_framework ./scleditor-react/public/ -ErrorAction Stop
 Remove-Item -rec -for ./publish
 Push-Location ./scleditor-react
 npm install
