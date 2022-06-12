0b26887f3aa0ba073fa0c7c6368ed190227a658a
antonyoni <antony@reduc.tech>
2022-06-12 13:31:05 +0100

    Update component name

--- scleditor-react/src/SCLPlaygroundHide.js
+++ scleditor-react/src/SCLPlaygroundHide.js
@@ -1,7 +1,9 @@
 import React, { createRef } from 'react';
 import { createPortal } from 'react-dom';
 
-class SCLPlayground extends React.Component {
+// Uses a React portal to hide the playground instead of unloading it each time
+
+class SCLPlaygroundHide extends React.Component {
   static playgroundId = 'scl-playground-root';
   static componentName = 'scl-playground-react';
   static portalRoot = 'sclplayground-portal';
@@ -10,7 +12,7 @@ class SCLPlayground extends React.Component {
     super(props);
 
     this.ref = createRef();
-    this.ref.current = document.getElementById(SCLPlayground.playgroundId);
+    this.ref.current = document.getElementById(SCLPlaygroundHide.playgroundId);
 
     this.state = {
       loading: this.ref.current == null,
@@ -49,47 +51,53 @@ class SCLPlayground extends React.Component {
   }
 
   static playgroundInit(component, params) {
-    const parentElement = document.querySelector(`#${SCLPlayground.playgroundId}`);
+    const parentElement = document.querySelector(`#${SCLPlaygroundHide.playgroundId}`);
     window.Blazor.rootComponents.add(parentElement, component, {});
   }
 
   blazorInit(callback) {
     if (!this.ref.current) {
       const pgRoot = document.createElement('div');
-      pgRoot.id = SCLPlayground.playgroundId;
+      pgRoot.id = SCLPlaygroundHide.playgroundId;
       pgRoot.style.display = 'none';
       document.body.appendChild(pgRoot);
       this.ref.current = pgRoot;
     }
 
-    window.sclPlaygroundInit = SCLPlayground.playgroundInit;
+    window.sclPlaygroundInit = SCLPlaygroundHide.playgroundInit;
 
-    SCLPlayground.addScript('_content/MudBlazor/MudBlazor.min.js')
+    SCLPlaygroundHide.addScript('_content/MudBlazor/MudBlazor.min.js')
       .then(() =>
-        SCLPlayground.addScript('_content/BlazorMonaco/lib/monaco-editor/min/vs/loader.js')
+        SCLPlaygroundHide.addScript('_content/BlazorMonaco/lib/monaco-editor/min/vs/loader.js')
       )
       .then(() => {
         const script = document.createElement('script');
         script.innerText = `require.config({ paths: { vs: '_content/BlazorMonaco/lib/monaco-editor/min/vs'}});`;
         document.body.appendChild(script);
-        SCLPlayground.addScript('_content/BlazorMonaco/jsInterop.js');
+        SCLPlaygroundHide.addScript('_content/BlazorMonaco/jsInterop.js');
       })
       .then(() =>
-        SCLPlayground.addScript(
+        SCLPlaygroundHide.addScript(
           '_content/BlazorMonaco/lib/monaco-editor/min/vs/editor/editor.main.js'
         )
       )
-      .then(() => SCLPlayground.addScript('_content/SCLEditor.Components/DefineSCLLanguage.js'))
-      .then(() => SCLPlayground.addScript('_framework/blazor.webassembly.js', { autostart: false }))
+      .then(() =>
+        SCLPlaygroundHide.addScript(
+          '_content/Reductech.Utilities.SCLEditor.Components/DefineSCLLanguage.js'
+        )
+      )
+      .then(() =>
+        SCLPlaygroundHide.addScript('_framework/blazor.webassembly.js', { autostart: false })
+      )
       // eslint-disable-next-line no-undef
       .then(() => Blazor.start().then(callback))
       .catch((error) => console.error(error));
   }
 }
 
-SCLPlayground.defaultProps = {
+SCLPlaygroundHide.defaultProps = {
   isDarkMode: false,
   loading: null,
 };
 
-export default SCLPlayground;
+export default SCLPlaygroundHide;
